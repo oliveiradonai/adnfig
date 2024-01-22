d94441b81adc375f97a1d253924a84e956b24258
oliveiradonai <adonai.fig@gmail.com>
2024-01-22 18:37:49 -0300

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
@@ -69,6 +70,7 @@ export default function RootLayout({
 			<body>
 				{children}
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
